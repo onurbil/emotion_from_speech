8be43fd74ff2d951f857fea223fe7f50aad61766
Onur Bilgin <onurbil@hotmail.com>
2021-02-19 11:29:30 +0100

    delete padding

--- dataset.py
+++ dataset.py
@@ -30,8 +30,7 @@ mfcc_number = 20
 # DATA_FOLDER = Path('../TESS Toronto emotional speech set data')
 class_dir = os.listdir(DATA_FOLDER)
 
-file_class = []
-features = np.zeros((0,mfcc_number,0))
+data_list = []
 for folder in class_dir:
     
     cls =  folder.split('_',1)[-1]
@@ -43,32 +42,23 @@ for folder in class_dir:
             
             file_path = os.path.join(cls_path, file)
             data, sr = librosa.load(file_path, sr=None)            
-            file_class.append([file,cls])
             
             if sr != sampling_rate:
                 # This loop is for OAF_food_fear.wav, which has sr of 96000 Hz.
                 data, sr = librosa.load(file_path, sr=sampling_rate)
                 
             mfccs = librosa.feature.mfcc(data, sr=sr, n_mfcc=20)
-            # Concatenate:
-            if features.shape[-1]>=mfccs.shape[-1]:
-                padded = np.zeros((features.shape[1],features.shape[-1]))
-                padded[:,:mfccs.shape[-1]] = mfccs
-                mfccs = padded
-            else:
-                padded = np.zeros((features.shape[0],features.shape[1],mfccs.shape[-1]))
-                padded[:,:,:features.shape[-1]] = features
-                features = padded
-            mfccs = np.expand_dims(mfccs, axis=0)
-            features = np.concatenate((features,mfccs),axis=0)
+            # For later first order and second order derivatives. 
+            # Energy not implemented. Chroma for music bc of harmonic, melodic features.
+            # mfcc_delta = librosa.feature.delta(mfcc, order=1)
+            # mfcc_delta2 = librosa.feature.delta(mfcc, order=2)
+            data_list.append([file,cls,mfccs])
+
                 
-file_class = np.array(file_class)
+print(data_list)
+print(len(data_list))
 
-print(features)
-print(features.shape)
 
-print(file_class)
-print(file_class.shape)
 
             
 
