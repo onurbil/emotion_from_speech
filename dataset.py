9a39c6aef38a48a0593ed192fee05b2c0f6d8039
Onur Bilgin <onurbil@hotmail.com>
2021-02-23 21:50:05 +0100

    energy

--- dataset.py
+++ dataset.py
@@ -49,15 +49,13 @@ for folder in class_dir:
                 # This loop is for OAF_food_fear.wav, which has sr of 96000 Hz.
                 data, sr = librosa.load(file_path, sr=sampling_rate)
                 
-            mfcc = librosa.feature.mfcc(data, sr=sr, n_mfcc=20)
-            # Bring to sequence x number of coefficients for LSTM:
-            mfcc=mfcc.T
+            mfcc = librosa.feature.mfcc(data, sr=sr, n_mfcc=20).T
 
-            
-            # For later first order and second order derivatives. 
-            # Energy not implemented. Chroma for music bc of harmonic, melodic features.
-            # mfcc_delta = librosa.feature.delta(mfcc, order=1)
-            # mfcc_delta2 = librosa.feature.delta(mfcc, order=2)
+        
+            # For later first order and second order and energy features. 
+            # mfcc_delta = librosa.feature.delta(mfcc, order=1).T
+            # mfcc_delta2 = librosa.feature.delta(mfcc, order=2).T
+            # energy = librosa.feature.rms(data).T
             data_list.append([mfcc,cls])
 
 print(data_list)                
